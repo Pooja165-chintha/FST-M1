98c97507fe0a0c2277382abb9be9b2dc5a23e4ca
Pooja165-chintha <66584045+Pooja165-chintha@users.noreply.github.com>
2021-05-27 22:46:28 +0530

    Add files via upload

---
+++ cucumber/TestRunner.java
@@ -0,0 +1,23 @@
+package runner;
+
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features ="Features",
+		glue = {"CucumberActivities"},
+		tags = "@SmokeTest",
+	//plugin= {"pretty"},
+		plugin= {"html:cucumber"},
+		monochrome=true
+	//	dryRun=true
+		
+				
+		)
+public class TestRunner {
+
+}
